f59c6888a27c8ad44d9a4d351f6d5ecbf63b00f9
Erik Webb <erik@erikwebb.net>
2012-11-29 09:13:19 -0500

    Add more basic Drupal-optimized VM

--- README.md
+++ README.md
@@ -11,9 +11,12 @@ First, build the Vagrant basebox using Veewee -
 ``
   vagrant basebox define 'CentOS-6.2-x86_64' 'CentOS-6.2-x86_64-minimal'
   vagrant basebox build CentOS-6.2-x86_64
+  vagrant basebox validate CentOS-6.2-x86_64
+  vagrant basebox export CentOS-6.2-x86_64
   vagrant box add ‘CentOS-6.2-x86_64′ ‘CentOS-6.2-x86_64.box’
 ``
 
 ## Todo ##
 
-- Replace MySQL with MariaDB
+1. Replace MySQL with MariaDB
+1. Create more Red Hat-friendly Apache Puppet module
---
+++ drupal-testbed/manifests/default.pp
@@ -0,0 +1,29 @@
+include base
+
+package { 'httpd':
+  provider => 'yum',
+}
+
+include apache
+include php, pear
+include mysql::server
+include varnish
+
+class { 'memcached':
+  max_memory => 128,
+}
+
+pear::package { "drush":
+  version => "5.4.0",
+  repository => "pear.drush.org",
+}
+
+# php::module { "xdebug": }
+# pear::package { "xhprof-0.9.2": }
+
+varnish::instance { "drupal":
+  backend      => "127.0.0.1:8080",
+  address      => ["127.0.0.1:80"],
+  admin_port   => "6082",
+  storage      => ["file,/var/varnish/storage1.bin,32M"],
+}
